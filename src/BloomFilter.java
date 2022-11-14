e5c490431eefbe5247b0d2e1551a0be34c1f0238
Otryaskin Andrey <kinlydog@gmail.com>
2022-11-15 04:04:07 +1000

    Update project

--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -1,2 +1,51 @@
+import java.util.BitSet;
+
 public class BloomFilter {
+    public int filter_len;
+    public int[] ma;
+
+    public BloomFilter(int f_len) {
+        filter_len = f_len;
+
+        ma = new int[filter_len];
+    }
+
+    public int hash(String str1, int num) {
+        int rand;
+        if (num == 2) rand = 223;
+        rand = 17;
+
+        int pref = 0;
+        int rez = 0;
+
+        for (int i = 1; i < str1.length(); i++) {
+            int code = (int) str1.charAt(i);
+
+            rez =  (pref * rand + code) % this.filter_len;
+
+            pref = rez;
+        }
+
+        return rez;
+    }
+    public int hash1(String str1) {
+        return hash(str1, 1);
+    }
+
+    public int hash2(String str1) {
+        return hash(str1, 2);
+    }
+
+    public void add(String str1) {
+        ma[hash1(str1)] = 1;
+        ma[hash2(str1)] = 1;
+    }
+
+    public boolean isValue(String str1) {
+        if (ma[hash1(str1)] == 1 && ma[hash2(str1)] == 1) {
+            return true;
+        }
+
+         return false;
+    }
 }
