7bce0640c044453e9f516b27d721e250b3024262
Otryaskin Andrey <kinlydog@gmail.com>
2022-11-19 13:53:13 +1000

    Update

--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -1,11 +1,10 @@
 public class BloomFilter {
     public int filter_len;
-    public int[] ma;
+    int filter;
 
     public BloomFilter(int f_len) {
         filter_len = f_len;
-
-        ma = new int[filter_len];
+        filter = 0;
     }
 
     public int hash(String str1, int rand) {
@@ -22,20 +21,76 @@ public class BloomFilter {
 
         return rez;
     }
+
     public int hash1(String str1) {
-        return hash(str1, 17);
+        int mask = 0;
+
+        return mask |= 1 << hash(str1, 17);
     }
 
     public int hash2(String str1) {
-        return hash(str1, 223);
+        int mask = 0;
+
+        return mask |= 1 << hash(str1, 223);
     }
 
     public void add(String str1) {
-        ma[hash1(str1)] = 1;
-        ma[hash2(str1)] = 1;
+        filter |= hash1(str1) | hash2(str1);
+    }
+
+    // don't work correctly
+    public boolean isValue(String str1) {
+        int t1 = hash1(str1);
+
+        if ((filter >> 28 & 1) == 1) {
+            System.out.println("!");
+        }
+
+        return (filter >> hash1(str1) & 1) == 1 && (filter >> hash2(str1) & 1) == 1;
+    }
+}
+
+/*public class BloomFilter {
+    public int filter_len;
+    int filter;
+
+    public BloomFilter(int f_len) {
+        filter_len = f_len;
+        filter = 0;
+    }
+
+    public int hash(String str1, int rand) {
+        int pref = 0;
+        int rez = 0;
+
+        for (int i = 1; i < str1.length(); i++) {
+            int code = (int) str1.charAt(i);
+
+            rez =  (pref * rand + code) % this.filter_len;
+
+            pref = rez;
+        }
+
+        return rez;
+    }
+
+    public int hash1(String str1) {
+        int mask = 0;
+
+        return mask |= 1 << hash(str1, 17);
+    }
+
+    public int hash2(String str1) {
+        int mask = 0;
+
+        return mask |= 1 << hash(str1, 223);
+    }
+
+    public void add(String str1) {
+        filter |= hash1(str1) | hash2(str1);
     }
 
     public boolean isValue(String str1) {
-        return ma[hash1(str1)] == 1 && ma[hash2(str1)] == 1;
+        return (filter >> hash1(str1) & 1) == 1 && (filter >> hash2(str1) & 1) == 1;
     }
-}
+}*/
